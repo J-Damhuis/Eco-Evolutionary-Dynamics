29ec5f7a3f54d79fc732cf3acf975d5ec10667f5
J-Damhuis <j.damhuis.1@student.rug.nl>
2019-03-20 16:24:13 +0100

    EOD 20-3

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@
 #include <fstream>
 #include <w32api/ntdef.h>
 
-const int n = 1000;
+const int n = 100;
 const int g = 100;
 const int d = 10;
 const double mu = 0.5;
 const double sigma = 0.01;
+const double beta = 1.0;
+const double s = 1.0;
 std::vector<double> MaxR = {10.0, 10.0};
 std::vector<double> R = MaxR;
 std::vector<std::vector<int> > Ind = {{}, {}};
 std::ofstream ofs("test.csv");
+std::ofstream ofs2("heatmap.csv");
 std::mt19937_64 rng;
 
 class Individual {
@@ -27,9 +30,9 @@ std::vector<Individual> createPopulation() {
     for (int i = 0; i < Population.size(); ++i) {
         Population[i].FeedEff = chooseValue(rng);
         Population[i].Food = 0.0;
-        std::cout << i << ": " << Population[i].FeedEff << "\n";
+        //std::cout << i << ": " << Population[i].FeedEff << "\n";
     }
-    std::cout << "\n";
+    //std::cout << "\n";
     return Population;
 };
 
@@ -40,13 +43,13 @@ void shuffle(std::vector<Individual> &Population) {
         Individual tmp = Population[i];
         Population[i] = Population[j];
         Population[j] = tmp;
-        std::cout << j << "\t";
+        //std::cout << j << "\t";
     }
-    std::cout << "\n\n";
+    //std::cout << "\n\n";
 }
 
 double calcEnergy(double x, int i) {
-    return exp(-1 * pow(x + pow(-1, i), 2));
+    return exp(-s * pow(x + pow(-1, i), 2));
 }
 
 void getFood(std::vector<Individual> &Population) {
@@ -57,10 +60,10 @@ void getFood(std::vector<Individual> &Population) {
         }
         for (int i = 0; i < Ind[j].size(); ++i) {
             Population[Ind[j][i]].Food += R[j] * calcEnergy(Population[Ind[j][i]].FeedEff, j) / Sum;
-            std::cout << Ind[j][i] << ": " << Population[Ind[j][i]].Food << "\n";
+            //std::cout << Ind[j][i] << ": " << Population[Ind[j][i]].Food << "\n";
         }
     }
-    std::cout << "\n";
+    //std::cout << "\n";
 }
 
 void chooseResource(std::vector<Individual> &Population) {
@@ -70,30 +73,40 @@ void chooseResource(std::vector<Individual> &Population) {
         shuffle(Population);
         R = MaxR;
         for (int i = 0; i < Population.size(); ++i) {
-            std::vector<double> Energy = {0.0, 0.0};
-            for (int j = 0; j < Energy.size(); ++j) {
-                for (int k = 0; k < Ind[j].size(); ++k) {
-                    Energy[j] += calcEnergy(Population[Ind[j][k]].FeedEff, j);
-                }
-                Energy[j] += calcEnergy(Population[i].FeedEff, j);
-                Energy[j] = R[j] * calcEnergy(Population[i].FeedEff, j) / Energy[j];
-            }
-            int j;
-            if (Energy[0] > Energy[1]) {
-                j = 0;
-            }
-            else if (Energy[0] == Energy[1]) {
-                j = Population[i].FeedEff < 0.0 ? 0 : 1;
+            std::uniform_real_distribution<double> chooseFraction(0.0, 1.0);
+            double r = chooseFraction(rng);
+            if (r > beta) {
+                double r2 = chooseFraction(rng);
+                int j = r2 > 0.5 ? 0 : 1;
+                Ind[j].push_back(i);
+                Sum[j] += Population[i].FeedEff;
+                Indiv[j] += 1.0;
+                //std::cout << i << ": " << j << "\n";
             }
             else {
-                j = 1;
+                std::vector<double> Energy = {0.0, 0.0};
+                for (int j = 0; j < Energy.size(); ++j) {
+                    for (int k = 0; k < Ind[j].size(); ++k) {
+                        Energy[j] += calcEnergy(Population[Ind[j][k]].FeedEff, j);
+                    }
+                    Energy[j] += calcEnergy(Population[i].FeedEff, j);
+                    Energy[j] = R[j] * calcEnergy(Population[i].FeedEff, j) / Energy[j];
+                }
+                int j;
+                if (Energy[0] > Energy[1]) {
+                    j = 0;
+                } else if (Energy[0] == Energy[1]) {
+                    j = Population[i].FeedEff < 0.0 ? 0 : 1;
+                } else {
+                    j = 1;
+                }
+                Ind[j].push_back(i);
+                Sum[j] += Population[i].FeedEff;
+                Indiv[j] += 1.0;
+                //std::cout << i << ": " << j << "\n";
             }
-            Ind[j].push_back(i);
-            Sum[j] += Population[i].FeedEff;
-            Indiv[j] += 1.0;
-            std::cout << i << ": " << j << "\n";
         }
-        std::cout << "\n";
+        //std::cout << "\n";
         getFood(Population);
         for (int i = 0; i < Ind.size(); ++i) {
             Ind[i].clear();
@@ -107,9 +120,9 @@ std::vector<double> getFitness(std::vector<Individual> &Population) {
     std::vector<double> Fitness(Population.size());
     for (int i = 0; i < Fitness.size(); ++i) {
         Fitness[i] = Population[i].Food;
-        std::cout << i << ": " << Population[i].Food << "\n";
+        //std::cout << i << ": " << Population[i].Food << "\n";
     }
-    std::cout << "\n";
+    //std::cout << "\n";
     return Fitness;
 }
 
@@ -142,10 +155,17 @@ void mutate(std::vector<Individual> &Population) {
 
 void simulate(std::vector<Individual> &Population) {
     for (int t = 0; t < g; ++t) {
+        ofs << t << ",";
+        ofs2 << t;
+        for (int i = 0; i < Population.size(); ++i) {
+            ofs2 << "," << Population[i].FeedEff;
+        }
+        ofs2 << "\n";
         chooseResource(Population);
         std::vector<double> Fitness = getFitness(Population);
         reproduce(Population, Fitness);
         mutate(Population);
+        std::cout << "Finished generation " << t << "\n";
     }
 }
 
@@ -154,7 +174,15 @@ int main() {
     if (!ofs.is_open()) {
         return 1;
     }
-    ofs << "NrR1,AvgR1,NrR2,AvgR2\n";
+    if (!ofs2.is_open()) {
+        return 1;
+    }
+    ofs << "Time,Resource 1,Resource 1,Resource 2,Resource 2\n";
+    ofs2 << "Time";
+    for (int i = 0; i < Population.size(); ++i) {
+        ofs2 << ",Individual " << i;
+    }
+    ofs2 << "\n";
     simulate(Population);
     return 0;
 }
