3ea2d8c0d9685f595cc1b3a3ec5764dafc5fbd7a
J-Damhuis <j.damhuis.1@student.rug.nl>
2019-03-25 17:14:43 +0100

    EOD 25-3

--- heatmap.R
+++ heatmap.R
@@ -1,12 +1,24 @@
 library(ggplot2)
 library(reshape)
-filename <- "heatmap.csv"
+library(gridExtra)
+heatmap <- "heatmap.csv"
+filename <- "test.csv"
 stepsize <- 0.05
 everyntimesteps <- 10
 
-makeheatmap <- function(filename, stepsize = 0.05, everyntimesteps = 1) {
+makeplots <- function(filename, heatmap, stepsize = 0.05, everyntimesteps = 1) {
   
-  d <- read.csv(filename, header = TRUE) #Create data frame of csv file
+  d <- read.csv(filename, header = TRUE) #Create data frame for lineplots of csv file
+  
+  plot1 <- ggplot(d, aes(x = Time)) + geom_line(aes(y = Resource.1, colour = "Resource 1")) + 
+    geom_line(aes(y = Resource.2, colour = "Resource 2")) + ylab("Fraction of population") + ylim(min = 0, max = 1) +
+    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) #Create lineplot for fractions of population feeding on resources
+  
+  plot2 <- ggplot(d, aes(x = Time)) + geom_line(aes(y = Resource.1.1, colour = "Resource 1")) + 
+    geom_line(aes(y = Resource.2.1, colour = "Resource 2")) + ylab("Mean X value") + ylim(min = -1, max = 1) +
+    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) #Create lineplot for mean X value of population feeding on resources
+  
+  d <- read.csv(heatmap, header = TRUE) #Create data frame for heatmap of csv file
   d2 <- as.data.frame(matrix(0, 2 / stepsize, (length(d[, 1]) - 1) / everyntimesteps + 1)) #Create data frame of 0s with 2/stepsize rows and as many columns as the number of time steps
   
   for(i in 1:length(d[, 1])) { #Each timestep
@@ -32,7 +44,9 @@ makeheatmap <- function(filename, stepsize = 0.05, everyntimesteps = 1) {
   d2$time <- times #Add new column to data frame with values of time points
   colnames(d2)[colnames(d2)=="value"] <- "count" #Rename value column to count
   
-  ggplot(d2, aes(time, X)) + geom_raster(aes(fill = count), interpolate = TRUE) #Create heat map
+  plot3 <- ggplot(d2, aes(time, X)) + geom_raster(aes(fill = count), interpolate = TRUE) #Create heatmap plot
+  
+  grid.arrange(plot1, plot2, plot3, ncol = 2) #Show all plots
 }
 
-makeheatmap(filename, stepsize, everyntimesteps)
+makeplots(filename, heatmap, stepsize, everyntimesteps)
