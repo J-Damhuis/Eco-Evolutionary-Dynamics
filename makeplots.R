ee15b2312921f18c060735d160d6e81e8eb15b7f
J-Damhuis <j.damhuis.1@student.rug.nl>
2019-03-26 13:56:46 +0100

    EOD 26-3

--- makeplots.R
+++ makeplots.R
@@ -4,7 +4,7 @@ library(gridExtra)
 heatmap <- "heatmap.csv"
 filename <- "test.csv"
 stepsize <- 0.05
-everyntimesteps <- 1
+everyntimesteps <- 100
 
 makeplots <- function(filename, heatmap, stepsize = 0.05, everyntimesteps = 1) {
   
@@ -12,17 +12,17 @@ makeplots <- function(filename, heatmap, stepsize = 0.05, everyntimesteps = 1) {
   
   plot1 <- ggplot(d, aes(x = Time)) + geom_line(aes(y = Resource.1, colour = "Resource 1")) + 
     geom_line(aes(y = Resource.2, colour = "Resource 2")) + ylab("Fraction of population") + ylim(min = 0, max = 1) +
-    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) #Create lineplot for fractions of population feeding on resources
+    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) + theme(legend.position = "top") #Create lineplot for fractions of population feeding on resources
   
-  png("plots.png")
-  print(plot1)
+  png("plots.png") #Create png file named plots
+  print(plot1) #Print plot1 onto plots.png; for some reason it changes to all three plots at the end of the function, which is fine with me
   
-  png("fraction.png")
-  print(plot1)
+  png("fraction.png") #Create png file named fraction
+  print(plot1) #Print plot1 onto fraction.png
   
   plot2 <- ggplot(d, aes(x = Time)) + geom_line(aes(y = Resource.1.1, colour = "Resource 1")) + 
     geom_line(aes(y = Resource.2.1, colour = "Resource 2")) + ylab("Mean X value") + ylim(min = -1, max = 1) +
-    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) #Create lineplot for mean X value of population feeding on resources
+    scale_colour_manual(values = c("Resource 1" = "blue", "Resource 2" = "red")) + theme(legend.position = "top") #Create lineplot for mean X value of population feeding on resources
   
   png("evolution.png")
   print(plot2)
@@ -31,7 +31,7 @@ makeplots <- function(filename, heatmap, stepsize = 0.05, everyntimesteps = 1) {
   d2 <- as.data.frame(matrix(0, 2 / stepsize, (length(d[, 1]) - 1) / everyntimesteps + 1)) #Create data frame of 0s with 2/stepsize rows and as many columns as the number of time steps
   
   for(i in 1:length(d[, 1])) { #Each timestep
-    if((i - 1) %% everyntimesteps == 0) { #Οnly use every everyntimesteps time step
+    if((i - 1) %% everyntimesteps == 0) { #Only use every everyntimesteps time step
       for(k in 1:length(d2[, 1])) { #Each stepsize
         for(j in 2:length(d[1, ])) { #Each individual
           if(d[i, j] <= k * stepsize - 1 & d[i, j] >= (k - 1) * stepsize - 1) { #If the X value is within current step
@@ -53,11 +53,11 @@ makeplots <- function(filename, heatmap, stepsize = 0.05, everyntimesteps = 1) {
   d2$time <- times #Add new column to data frame with values of time points
   colnames(d2)[colnames(d2)=="value"] <- "count" #Rename value column to count
   
-  plot3 <- ggplot(d2, aes(time, X)) + geom_raster(aes(fill = count), interpolate = TRUE) #Create heatmap plot
+  plot3 <- ggplot(d2, aes(time, X)) + geom_raster(aes(fill = count), interpolate = TRUE) + theme(legend.position = "top") #Create heatmap plot
   
-  png("heatmap.png")
-  print(plot3)
-  dev.off()
+  png("heatmap.png") #Create png file named heatmap
+  print(plot3) #Print plot3 onto heatmap.png
+  dev.off() #Close png file
   
   grid.arrange(plot1, plot2, plot3, ncol = 2) #Show all plots
 }
